db75b914220d51fe36b0beeeed59d50eab2bfab0
Atif Hussain <67437879+atif-hu@users.noreply.github.com>
2022-03-01 22:29:04 +0530

    Add files via upload

---
+++ Controllers/LandlordController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LoginApplication.Models;
+using LoginApplication.Models.LM;
+
+namespace LoginApplication.Controllers
+{
+    [RoutePrefix("Api/Landlord")]
+    public class LandlordController : ApiController
+    {
+        RenthutEntities3 DB = new RenthutEntities3();
+        [Route("InsertHouseDetails")]
+        [HttpPost]
+        public object InsertHouseDetails(Landlord obj)
+        {
+            landlord ll = new landlord();
+            try
+            {
+
+                ll.File_name = obj.File_name;
+                ll.House_description = obj.House_description;
+                ll.Address = obj.Address;
+                ll.City = obj.City;
+                ll.Rent_price = obj.Rent_price;
+                ll.Lease_terms = obj.Lease_terms;
+                ll.Deposit = obj.Deposit;
+                ll.Ad_date = obj.Ad_date;
+                DB.landlords.Add(ll);
+                DB.SaveChanges();
+                return new Response { Status = "Success", Message = "Data Successfully" };
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return new Response
+            {
+                Status = "Error",
+                Message = "Data not insert"
+            };
+        }
+
+        [Route("HouseDetails")]
+        public IEnumerable<landlord> Get()
+        {
+            List<landlord> ls = DB.landlords.ToList();
+            return ls;
+        }
+
+        //[Route("HousedetailById")]
+        //[HttpGet]
+        //public object HousedetailById(int id)
+        //{
+        //    //var obj = DB.landlords.Where(x => x.Id == id).ToList().FirstOrDefault();
+        //    var obj = DB.landlords.Find(id);
+        //    return obj;
+        //}
+
+        public landlord Get(int id)
+        {
+            landlord prd = DB.landlords.Find(id);
+            return prd;
+        }
+
+        [Route("UpdateHouseDetails")]
+        public object UpdateHouseDetails(landlord obj)
+        {
+            try
+            {
+                var ll = DB.landlords.Where(x => x.Id == obj.Id).ToList().FirstOrDefault();
+                //landlord ll = DB.landlords.Find(obj.Id);
+                ll.File_name = obj.File_name;
+                ll.House_description = obj.House_description;
+                ll.Address = obj.Address;
+                ll.City = obj.City;
+                ll.Rent_price = obj.Rent_price;
+                ll.Lease_terms = obj.Lease_terms;
+                ll.Deposit = obj.Deposit;
+                ll.Ad_date = obj.Ad_date;
+                DB.SaveChanges();
+                return new Response
+                {
+                    Status = "Updated",
+                    Message = "Updated Successfully"
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return new Response
+            {
+                Status = "Error",
+                Message = "Data not insert"
+            };
+        }
+
+
+        public void Delete(int id)
+        {
+            landlord ll= DB.landlords.Find(id);
+            DB.landlords.Remove(ll);
+            DB.SaveChanges();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LoginApplication.Models;
+using LoginApplication.Models.VM;
+
+namespace LoginApplication.Controllers
+{
+    [RoutePrefix("Api/login")]
+    public class LoginController : ApiController
+    {
+        RenthutEntities3 DB = new RenthutEntities3();
+        [Route("InsertEmployee")]
+        [HttpPost]
+        public object InsertEmployee(Register Reg)
+        {
+            try
+            {
+                EmployeeLogin EL = new EmployeeLogin();
+                if (EL.Id == 0)
+                {
+                    EL.EmployeeName = Reg.EmployeeName;
+                    EL.City = Reg.City;
+                    EL.Email = Reg.Email;
+                    EL.Password = Reg.Password;
+                    EL.Age = Reg.Age;
+                    DB.EmployeeLogins.Add(EL);
+                    DB.SaveChanges();
+                    return new Response { Status = "Success", Message = "Record SuccessFully Saved." };
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return new Response { Status = "Error", Message = "Invalid Data." };
+        }
+        [Route("Login")]
+        [HttpPost]
+        public Response employeeLogin(Login login)
+        {
+            var log = DB.EmployeeLogins.Where(x => x.Email.Equals(login.Email) && x.Password.Equals(login.Password)).FirstOrDefault();
+            var adminuser = "admin@renthut.com";
+            var adminpass = "admin";
+            if(login.Email==adminuser && login.Password == adminpass)
+            {
+                return new Response { Status = "Admin", Message = "Admin login successfully" };
+            }
+            else if (log == null)
+            {
+                return new Response { Status = "Invalid", Message = "Invalid User." };
+            }
+            else
+                return new Response { Status = "Success", Message = "Login Successfully" };
+        }
+
+        [Route("EmployeeDetails")]
+        public IEnumerable<EmployeeLogin> Get()
+        {
+            List<EmployeeLogin> ls = DB.EmployeeLogins.ToList();
+            return ls;
+        }
+
+        public void Delete(int id)
+        {
+            EmployeeLogin ll = DB.EmployeeLogins.Find(id);
+            DB.EmployeeLogins.Remove(ll);
+            DB.SaveChanges();
+        }
+    }
+}
